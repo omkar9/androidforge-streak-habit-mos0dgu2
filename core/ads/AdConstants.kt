7db75292d61ed90b61ec56f9aec7bbb5f0e5745f
omkar9 <omkar946@gmail.com>
2026-05-05 07:59:21 +0530

    feat: add AdConstants.kt

---
+++ core/ads/AdConstants.kt
@@ -0,0 +1,9 @@
+package com.androidforge.streakhappit.core.ads
+
+object AdConstants {
+    // Use test ad unit IDs for development
+    const val BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111" // Test Banner
+    const val INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712" // Test Interstitial
+
+    // TODO: Replace with your actual AdMob ad unit IDs before publishing
+}
