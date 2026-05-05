fa35c0fc916ccf95c8687ca87af0e2f4179eccd3
omkar9 <omkar946@gmail.com>
2026-05-05 07:59:07 +0530

    feat: add Constants.kt

---
+++ core/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.androidforge.streakhappit.core.common
+
+object Constants {
+    const val NOTIFICATION_CHANNEL_ID = "habit_reminder_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Habit Reminders"
+    const val NOTIFICATION_ID_BASE = 1000
+    const val ANIMATION_DURATION = 300 // Milliseconds
+    const val DATABASE_NAME = "streak_habit_db"
+}
