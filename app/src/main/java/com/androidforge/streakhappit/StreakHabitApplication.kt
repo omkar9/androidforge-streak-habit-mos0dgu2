ddb4c72e8e5b4ce511ed75006fdf5ee73c58deee
omkar9 <omkar946@gmail.com>
2026-05-05 08:00:50 +0530

    fix: update StreakHabitApplication.kt

--- app/src/main/java/com/androidforge/streakhappit/StreakHabitApplication.kt
+++ app/src/main/java/com/androidforge/streakhappit/StreakHabitApplication.kt
@@ -1,24 +1,7 @@
 package com.androidforge.streakhappit
 
 import android.app.Application
-import com.androidforge.streakhappit.core.ads.AdMobManager
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
-class StreakHabitApplication : Application() {
-
-    @Inject
-    lateinit var adMobManager: AdMobManager
-
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-
-        // Initialize AdMob SDK early
-        adMobManager.initialize()
-    }
-}
+class StreakHabitApplication : Application()
